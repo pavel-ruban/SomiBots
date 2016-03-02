a648de43f647ad736185cd11ac957ca8c0ae06af
Pavel Ruban <pruban@adyax.com>
2016-03-02 14:09:55 +0600

    refs [#31928] Slack bot implementation v. 0.0.1

---
+++ bin/bot.js
@@ -0,0 +1,17 @@
+# bin/bot.js
+
+'use strict';
+
+var NorrisBot = require('../lib/somi_bot');
+
+var token = process.env.BOT_API_KEY;
+var dbPath = process.env.BOT_DB_PATH;
+var name = process.env.BOT_NAME;
+
+var somibot = new SomiBot({
+  token: token,
+  dbPath: dbPath,
+  name: name
+});
+
+somibot.run();
